dca0bbe279e0e06891b3232569ccba880b341f9d
Dmitry <qventymr@gmail.com>
2024-02-21 18:55:47 +0300

    1.0

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextToSpeech textToSpeech;
+    private TextView result_text;
+    private EditText editText;
+    private Button button;
+    private String text;
+    private String finish_text;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy =
+                    new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        result_text = findViewById(R.id.text);
+        editText = findViewById(R.id.EditTextPole);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editText.getText().toString().equals("")) {
+                    Toast.makeText(MainActivity.this, R.string.textNoUserInput, Toast.LENGTH_LONG).show();
+                }
+                else {
+                    text = editText.getText().toString();
+                    try {
+                        finish_text = translate("ru", "en", text);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    result_text.setText(finish_text);
+                }
+            }
+        });
+
+    }
+    @NonNull
+    private static String translate(String langFrom, String langTo, String text) throws IOException{
+        // INSERT YOU URL HERE
+        String urlStr = "https://script.google.com/macros/s/AKfycbyGT-0mhCXm1zRHtxcUeFMBPY2bZOSwrgTv34sIe_HyUQLyHmV4siZCBv8wGLgOWumJ/exec" +
+                "?q=" + URLEncoder.encode(text, "UTF-8") +
+                "&target=" + langTo +
+                "&source=" + langFrom;
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+}
